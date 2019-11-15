549258db7c11aea1310847846778ee22d4f941fc
Yoganand Chandrasekaran <yorganand@gmail.com>
2019-11-16 01:47:58 +0530

    complete

--- js/ratings.js
+++ js/ratings.js
@@ -11,8 +11,19 @@ function collect_ratings() {
     let rating = 0;
 
     elements.forEach(element => {
-
+        rating = Number.parseInt(element.id.replace('star',''));
+        const elementValue = Number.parseInt(element.value);
+        if(elementValue !== 0) {
+            ratings.count = ratings.count + elementValue;
+            ratings.sum = ratings.sum + (rating * elementValue);
+            ratings.average = ratings.sum / ratings.count;
+        }
     });
 
-
+    return ratings;
 }
+
+document.addEventListener('change', (event) => {
+    const ratings = collect_ratings();
+    document.getElementById('average').value = ratings.average.toPrecision(2);
+})
