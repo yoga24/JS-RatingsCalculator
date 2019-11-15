5146e224ff39437e9836b2894441023e93817cf2
Yoganand Chandrasekaran <yorganand@gmail.com>
2019-11-16 02:03:29 +0530

    fix test failures

--- js/ratings.js
+++ js/ratings.js
@@ -1,3 +1,5 @@
+console.log("JS loaded!");
+
 function collect_ratings() {
 
     const elements = document.querySelectorAll('.rating');
@@ -5,17 +7,16 @@ function collect_ratings() {
     const ratings = {
         count: 0,
         sum: 0,
-        average = 0
+        average: 0
     };
 
     let rating = 0;
 
     elements.forEach(element => {
-        rating = Number.parseInt(element.id.replace('star',''));
-        const elementValue = Number.parseInt(element.value);
-        if(elementValue !== 0) {
-            ratings.count = ratings.count + elementValue;
-            ratings.sum = ratings.sum + (rating * elementValue);
+        rating = parseInt(element.id.replace('star',''));
+        ratings.count = ratings.count + parseInt(element.value);
+        if(ratings.count !== 0) {
+            ratings.sum = ratings.sum + (rating * parseInt(element.value));
             ratings.average = ratings.sum / ratings.count;
         }
     });
@@ -25,5 +26,5 @@ function collect_ratings() {
 
 document.addEventListener('change', (event) => {
     const ratings = collect_ratings();
-    document.getElementById('average').value = ratings.average.toPrecision(2);
+    document.querySelector('#average').value = ratings.average.toFixed(2);
 })
