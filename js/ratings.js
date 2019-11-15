3603a71a9c59ab12b2d8034a3b25b29e8d0bb1ef
Yoganand Chandrasekaran <yorganand@gmail.com>
2019-11-16 01:07:56 +0530

    step1-6

---
+++ js/ratings.js
@@ -0,0 +1,18 @@
+function collect_ratings() {
+
+    const elements = document.querySelectorAll('.rating');
+
+    const ratings = {
+        count: 0,
+        sum: 0,
+        average = 0
+    };
+
+    let rating = 0;
+
+    elements.forEach(element => {
+
+    });
+
+
+}
